0eb1ce2cbc842562c71b1131972d07097b1c3f22
xuanlam <phamxuanlam@gmail.com>
2022-03-11 16:24:12 +0700

    Droid Cafe Input Starter Project

--- app/src/main/java/com/example/android/droidcafe/MainActivity.java
+++ app/src/main/java/com/example/android/droidcafe/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Checkable;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
